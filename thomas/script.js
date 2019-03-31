cf297d10b3a021dc05f13c56323d96122236d195
SavvasStephanides <savvascyp@hotmail.com>
2019-03-31 10:53:21 +0100

    Add GIFs

---
+++ thomas/script.js
@@ -0,0 +1,32 @@
+addDialerEvents();
+
+function addDialerEvents(){
+    var dialerButtons = document.querySelectorAll(".button a");
+    dialerButtons.forEach(function(eachButton){
+        eachButton.addEventListener("click", function(){
+            var gif = eachButton.getAttribute("gif");
+            if(gif == null){
+                setRandomGif();
+            }
+            else{
+                setGif(gif);
+            }
+            
+        });
+    });
+}
+
+function setRandomGif(){
+    var allGifs = [];
+    document.querySelectorAll('[gif]').forEach(function(eachGif){
+        allGifs.push(eachGif.getAttribute("gif"));
+    });
+
+    var random = Math.floor(Math.random() * 5);
+    setGif(allGifs[random]);
+}
+
+function setGif(gifFilename){
+    document.body.style.background = "url('images/" + gifFilename + "') no-repeat center center fixed";
+    document.body.style.backgroundSize = "cover";
+}
